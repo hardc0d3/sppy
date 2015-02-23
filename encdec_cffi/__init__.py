2ab1efb9b5bf18c5d315faedc86f78496f7d575a
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-02-23 08:25:45 +0200

    play with data protocol in encdec

--- encdec_cffi/__init__.py
+++ encdec_cffi/__init__.py
@@ -1,41 +1,67 @@
+import cffi
+
+_cdef = """
+void *malloc(size_t size);
+void free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+void *memcpy(void *dest, const void *src, size_t n);
+
+typedef struct {
+  uint8_t tp;
+  uint16_t sz;
+  const char *data;
+} s16;
+
+"""
 
 
 class EncoderDecoder(object):
 
-    def __init__( self, ffi ):
-        self.ffi = ffi
+    def __init__( self ):
+        self.ffi = cffi.FFI()
+        self.ffi.cdef (_cdef)
+        self.lib = self.ffi.dlopen(None)
+        self._TYPE_STR = 99 
+        self._TYPE_SZ = 1
+        self._MAXLEN_TYPE = 1
+        self._MAXLEN_STR = 2
+        
 
     def enc_str( self, s ):
-        return self.ffi.new( 'char []',s )
+        sz = self._TYPE_SZ+self._MAXLEN_STR+len(s)
+        cd = self.ffi.new('unsigned char *')
+        bu = self.ffi.buffer(cd,sz)
+        ct = self.ffi.new('uint8_t *',self._TYPE_STR)
+        bt = self.ffi.buffer( ct,self._TYPE_SZ)
+        
+        bu[0] = bt[0] 
+        clen = self.ffi.new( 'uint16_t*')
+        clen[0] = len(s)
+        clen_bu = self.ffi.buffer(clen,self._MAXLEN_STR)
+         
+        print "clen",clen[0] 
+        bu[1] = clen_bu[0]
+        bu[2] = clen_bu[1]
+        ofs = 3
+        for c in s: 
+            bu[ofs] = c
+            ofs+=1
+        return cd
 
-    def dec_str( self, cs ):
-        return self.ffi.string( self, cs )
+    def dec_str( self, cd ):
+        meta_bu = self.ffi.buffer( cd,3) #!!! 
+        clen = self.ffi.new( 'uint16_t*')
+        tp = self.ffi.new( 'uint8_t*')
+        tp_bu  = self.ffi.buffer( tp,self._TYPE_SZ)
+        clen_bu = self.ffi.buffer(clen,self._MAXLEN_STR)
+        
+        tp_bu[0] = meta_bu[0]        
+        clen_bu[0]=meta_bu[1]
+        clen_bu[1]=meta_bu[2]
 
-    def enc_utf8( self, u8 ):
-        return self.ffi.new ( 'char []', u8.encode('utf-8') )
-
-    def dec_utf8( self, cu8 ):
-        return self.ffi.string ( cu8 ).decode('utf-8')
-
-    def enc_num( self,typestr, num):
-        return self.ffi.cast(typestr,num)
-
-
-    def enc_unum( self, typestr ,num ):
-        if ( num >= 0 ):
-             return self.ffi.cast("unsigned %s" % typestr , num)
-        else:
-             raise Exception ("must be >=0 ")
-
-    def dec_int( self, cnum ):
-             return int( cnum )
-  
-    def dec_long( self,cnum):
-             return int ( cnum )
-
-    def dec_float( self,cnum):
-             return float( cnum ) 
+        data_bu = self.ffi.buffer( cd, 3 + clen[0])
+        return (tp[0],clen[0]-1,data_bu[3:] )
 
 
 
-   
--- test_encdec_cffi.py
+++ test_encdec_cffi.py
@@ -1,21 +1,11 @@
-
-import cffi
 from encdec_cffi import EncoderDecoder as ED
 
-ffi = cffi.FFI()
-ed = ED(ffi)
+ed = ED()
+
+c = ed.enc_str("some very long string")
+
+print c
 
 
-i = int(134123)
-l = long(12341234134132)
-f = float(11234.12341234132)
-
-
-print "uint",i,ed.enc_unum("int",i)
-print "ulong",l,ed.enc_unum("long",l)
-print "double",f,ed.enc_num("double",f)
-
-
-
-
+print ed.dec_str(c)
 
