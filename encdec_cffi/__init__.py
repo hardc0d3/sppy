5c661fa4043ea43d540a4dcdbac9afb684b46b3a
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-02-25 17:28:56 +0200

    cffi string encoder easy type protocol

--- encdec_cffi/__init__.py
+++ encdec_cffi/__init__.py
@@ -7,12 +7,6 @@ void *calloc(size_t nmemb, size_t size);
 void *realloc(void *ptr, size_t size);
 void *memcpy(void *dest, const void *src, size_t n);
 
-typedef struct {
-  uint8_t tp;
-  uint16_t sz;
-  const char *data;
-} s16;
-
 """
 
 
@@ -22,46 +16,57 @@ class EncoderDecoder(object):
         self.ffi = cffi.FFI()
         self.ffi.cdef (_cdef)
         self.lib = self.ffi.dlopen(None)
-        self._TYPE_STR = 99 
+        self._TYPE_STR = 99
+        self._TYPE_UTF8 = 100 
         self._TYPE_SZ = 1
         self._MAXLEN_TYPE = 1
         self._MAXLEN_STR = 2
-        
 
-    def enc_str( self, s ):
-        sz = self._TYPE_SZ+self._MAXLEN_STR+len(s)
-        cd = self.ffi.new('unsigned char *')
-        bu = self.ffi.buffer(cd,sz)
-        ct = self.ffi.new('uint8_t *',self._TYPE_STR)
-        bt = self.ffi.buffer( ct,self._TYPE_SZ)
-        
-        bu[0] = bt[0] 
-        clen = self.ffi.new( 'uint16_t*')
-        clen[0] = len(s)
-        clen_bu = self.ffi.buffer(clen,self._MAXLEN_STR)
-         
-        print "clen",clen[0] 
-        bu[1] = clen_bu[0]
-        bu[2] = clen_bu[1]
-        ofs = 3
-        for c in s: 
-            bu[ofs] = c
-            ofs+=1
-        return cd
+   
+    def enc_str( self, s, itp ):
+        sc="xxxx"+s
+        cdata = self.ffi.new("char []",sc)
+        ctype = self.ffi.new('uint8_t *',itp)
+        clen  = self.ffi.new('uint16_t *',len(s) )
+        print cdata
+        typebuff =  self.ffi.buffer(ctype,2)        
+        lenbuff  =  self.ffi.buffer(clen, 2)
+        databuff = self.ffi.buffer(cdata,len(sc))
+        databuff[1] = typebuff [0]
+        databuff[2] = lenbuff  [0]
+        databuff[3] = lenbuff  [1]
 
-    def dec_str( self, cd ):
-        meta_bu = self.ffi.buffer( cd,3) #!!! 
-        clen = self.ffi.new( 'uint16_t*')
-        tp = self.ffi.new( 'uint8_t*')
-        tp_bu  = self.ffi.buffer( tp,self._TYPE_SZ)
-        clen_bu = self.ffi.buffer(clen,self._MAXLEN_STR)
-        
-        tp_bu[0] = meta_bu[0]        
-        clen_bu[0]=meta_bu[1]
-        clen_bu[1]=meta_bu[2]
+        for c in databuff:
+            print ('enc',c)
+        return cdata
 
-        data_bu = self.ffi.buffer( cd, 3 + clen[0])
-        return (tp[0],clen[0]-1,data_bu[3:] )
+    def dec_str( self, cdata ):
+        buff = self.ffi.buffer ( cdata, 4 )
+        ctype = self.ffi.new('uint8_t*',0 )
+        clen  = self.ffi.new('uint16_t*',2 )
+        lenbuff = self.ffi.buffer ( clen, 2 )
+        typebuff = self.ffi.buffer ( ctype, 1 )
+        typebuff[0] = buff [1]
+        lenbuff[0] = buff [2]
+        lenbuff[1] = buff [3]         
+        buff = self.ffi.buffer ( cdata, clen[0]+4 ) 
+        for c in buff:
+            print ('dec',c)
+ 
+        return ctype[0],clen[0],buff[4:]
 
+
+
+    def enc_utf8( self, u8 ):
+        st = u8.encode('utf-8')
+        print ("encu8",u8,st,len(st))
+        #print "St",st
+        return self.enc_str(st,self._TYPE_UTF8)
+
+
+
+    def dec_utf8( self, cd):
+        r = self.dec_str( cd )
+        return r
 
 
--- test_encdec_cffi.py
+++ test_encdec_cffi.py
@@ -1,11 +1,26 @@
+# -*- coding: utf-8 -*-
+
 from encdec_cffi import EncoderDecoder as ED
 
 ed = ED()
 
-c = ed.enc_str("some very long string")
-
+c = ed.enc_str("azbuka",ed._TYPE_STR)
 print c
+print "----" 
+print ed.dec_str(c)
 
+
+uni = unicode("азбука","utf-8")
 
 print ed.dec_str(c)
+
+cu = ed.enc_utf8(uni)
+
+print cu
+
+
+r = ed.dec_utf8(cu)
+
+print r,r[2]
+
 
