47fb33070b46b06036410713cbd3d6700766b7c7
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-02-28 19:17:30 +0200

    spapi cffi and wrap

--- _spapi_cffi.py
+++ _spapi_cffi.py
@@ -4,40 +4,71 @@ _spapi_cffi
 Py cffi wrapper Copyright (c) Dobri Stoilov
 BSD License
 '''
-
+from numbers import Number
 import cffi
+
 from _spapi import SpApi
 null=None
 
 class NTMBS(object):
      def __init__(self,ffi):
          self.ffi = ffi
+
      def decode( self,nts, sz ):
          return self.ffi.string( self.ffi.cast ("char*",nts ) ) 
+
+     def encode( self, s):
+         return self.ffi.new("char[]",s)
+
+class CastIntCodec(object):
+     def __init__(self,ffi):
+         self.ffi = ffi
+
+     def decode( self,ci, sz ):
+         return int(ci) 
+
+     def encode( self, i):
+         return self.ffi.cast("int",i)
+
 
 
 class Wrap(object):
     """ cffi & sophia object wrapper """
     def __init__(self, sp, codec, fun, args ):
-        #print "args",args
-        #print "fun", fun
         self.sp = sp
-        self.ret = self.sp.ffi.NULL
-        self.ret_sz = 0
+        self.cd  = self.sp.ffi.NULL
+        self.cd_sz = 0
         self.codec = codec
-        #print self.ret
-        if len(args) == 0:
-            self.ret = fun() 
-        newargs = tuple([ x.ret for x in args ])
-        #print "newargs",newargs
-        self.ret = fun( *newargs )
-        #print "ret",self.ret
         self._ = self.decode
+        self.cdargs = []
+
+        for arg in args:
+            if isinstance(arg,Wrap):
+                self.cdargs.append(arg.cd)
+            if isinstance(arg,sp.ffi.CData):
+                self.cdargs.append(arg)
+            if isinstance(arg, Number):
+                if arg >= 0:
+                    # wrap ( cdata ( encode unum
+                    pass
+                    #self.unsigned_cdargs.append(arg)
+                else: 
+                    #self.signed_cdargs.append(arg)
+                    pass
+            if isinstance(arg, str ):
+                    self.cdargs.append( NTMBS( sp.ffi ).encode( arg ) ) 
+                    pass
+        print self.cdargs
+        if len(self.cdargs) == 0:
+            self.cd = fun()
+        else:
+            self.cd = fun( *self.cdargs)
+        
 
     def decode(self):
-        if self.codec is None or self.ret == self.sp.ffi.NULL: 
+        if self.codec is None or self.cd == self.sp.ffi.NULL: 
             return None
-        return self.codec.decode ( self.ret, self.ret_sz )
+        return self.codec.decode ( self.cd, self.cd_sz )
 
 
 
@@ -53,7 +84,8 @@ class SpApiFFI(SpApi):
         self.lib = self.f.dlopen( dl )
         self.null = self.f.NULL
         self.codec_ntmbs = NTMBS(self.ffi) 
-    
+        self.codec_castint = CastIntCodec(self.ffi)
+ 
     def env(self, *args):
                    # sp,  codec,  fun,           args
         return Wrap( self, None, self.lib.sp_env, args)
@@ -65,16 +97,19 @@ class SpApiFFI(SpApi):
         return Wrap(self,None, self.lib.sp_object , args )
 
     def open( self, *args):
-        return self.lib.sp_open( *args )
+       #            sp    codec              fun                 args
+       return Wrap(self, self.codec_castint, self.lib.sp_open,   args )
 
     def destroy( self, *args):
-        return self.lib.sp_destroy( *args )
+       #            sp    codec               fun                  args
+        return Wrap(self, self.codec_castint, self.lib.sp_destroy, args )
     
     def error( self, *args):
         return self.lib.sp_error( *args) 
 
     def set ( self, *args ):
-        return self.lib.sp_set( *args )
+        #            sp    codec    fun             args
+        return Wrap(self, self.codec_castint , self.lib.sp_set ,args )
  
     def delete(self, *args):
         return self.lib.sp_delete( *args) 
--- test/test_cffi_wrap.py
+++ test/test_cffi_wrap.py
@@ -1,4 +1,3 @@
-
 import sys
 sys.path.append('../')
 sys.path.append('../cfficodecs')
@@ -11,12 +10,19 @@ sp_dl = '../build/sp.so'
 
 sp = SpApiFFI( _spapi_cdef.cdef, sp_dl )
 
-
 env = sp.env()
-sptype = sp.type(env)
-print sptype.decode()
+print "env:",env.cd
+typ = sp.type(env)
+print "typ",typ.decode()
 ctl = sp.ctl(env)
-sptype = sp.type(ctl)
-"decode shortland is _"
-print sptype._()
+typ = sp.type(ctl)
+print "typ",typ.decode()
+rc = sp.set( ctl, "sophia.path", "../test_data/" ) 
+print "set ctl path", rc.decode()
+rc = sp.set( ctl, "db", "spwrap" )
+print "set ctl db name",rc
+rc = sp.open( env )
+print "open env",rc._()
+rc = sp.destroy( env)
+print "close env",rc._()
 
