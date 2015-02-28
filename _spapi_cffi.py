898016e16088725e6d4bc5731c49986d2e5445e6
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-02-28 09:51:09 +0200

    just *args

--- _spapi_cffi.py
+++ _spapi_cffi.py
@@ -14,77 +14,54 @@ null=None
 class SpApiFFI(SpApi):
     """ sophia api wrapper """
 
-
-
-    def __init__( self, cdef, sodll ):
+    def __init__( self, cdef, dl ):
         self.ffi = cffi.FFI() 
         self.f = self.ffi
         self.cdef = cdef
         self.f.cdef( self.cdef )
-        self.lib = self.f.dlopen( sodll )
-        global null
-        null = self.f.NULL
+        self.lib = self.f.dlopen( dl )
+        self.null = self.f.NULL
     
     def env(self):
         return self.lib.sp_env()
 
-    def ctl(self, env=null):
-        if env != null: 
-            return self.lib.sp_ctl( env )
-        return null
+    def ctl(self, *args ):
+        return self.lib.sp_ctl( *args )
 
-    def object( self, db=null, cursor=null ):
-        #
-        #return self.c.sp_object( db or cursor )
-        pass
-    def open( self, db = null, env = null):
-        #print "d: open, db, env:", db,env
-        if env != null and db == None:
-            return self.lib.sp_open( env )
-        if db != null and env == None:
-            return self.lib.sp_open( db )
+    def object( self, *args ):
+        return self.lib.sp_object ( *args )
 
-    def destroy( self, res = null):
-        if res != null:
-            return self.lib.sp_destroy( res )
+    def open( self, *args):
+        return self.lib.sp_open( *args )
+
+    def destroy( self, *args):
+        return self.lib.sp_destroy( res )
     
-    def error( self, env = null):
-        pass
-    def set( self, ctl = null, ctlkey = null, ctlval = null, 
-             db = null, obj = null,  
-             transaction = null,
-             field = null, ptr =null, size =null   ):
+    def error( self, *args):
+        return self.lib.sp_error( *args) 
 
-        if ctl != null and ctlkey != null and ctlval != null:
-            return self.lib.sp_set( ctl, ctlkey, ctlval )
-  
-
-        return self.f.cast( "int",-1)
+    def set ( self, *args ):
+        return self.lib.sp_set( *args )
  
-    def delete(self, db = null, obj = null, transaction = null):
-        pass
-    def get(self, ctl = null, ctlkey = null, db = null,
-            obj = null, transaction = null,
-             field  = null, size = null):
+    def delete(self, *args):
+        return self.lib.sp_delete( *args) 
 
-        if ctl !=null and ctlkey !=null:
-            return self.lib.sp_get(ctl,ctlkey) 
+    def get( self, *args ):
+        return self.lib.sp_get( *args )
+ 
+    def drop(self, *args):
+        return self.lib.sp_drop( *args)
+    
+    def cursor(self, *args):
+        return self.lib.sp_cursor( *args)
+ 
+    def begin(self, *args):
+        return self.lib.sp_begin( *args)
+ 
+    def commit(self, *args):
+        return self.lib.sp_commit( *args)
 
-        pass
-    # delete snapshot or close db := destroy(res=db)
-    def drop(self, snapshot = null, db = null):
-        pass
-    def cursor(self, ctl = null, db = null, obj = null):
-        pass
-    def begin(self, env = null):
-        pass
-    def commit(self, transaction = null):
-        pass
-    # res := resource should sophia:
-    # env, ctl, db, obj, snapshot,
-    # transaction, cursor
-    def sptype(self, res=null):
-        if res != null:
-            return self.lib.sp_type(res)
-        pass
+    def type( self, *args):
+        return self.lib.sp_type( *args)
+
 
--- test/test_cffi_spapi.py
+++ test/test_cffi_spapi.py
@@ -6,94 +6,43 @@ import _spapi_cdef
 from _spapi_cffi import SpApiFFI
 from cfficodec import CharString
 
+global sp_sodll, SP_PATH, sp_path, SP_DB, sp_db, sp_db_key
+SP_PATH = 'sophia.path'
+sp_path = '../test_data/spapi_env'
+SP_DB = 'db'
+sp_db = 'spapi_cffi_test'
+db_key = SP_DB + "." +  sp_db
+sp_dl = '../build/sp.so'
 
-SP_PATH = "sophia.path"
-sp_path = "../test_data/sp_test_env1"
-SP_DB = "db"
-db_name = "env1test"
-db_key = SP_DB+"."+db_name
-
-def sp_type( sp,vp ):
-    return sp.f.string(sp.f.cast("char *",sp.sptype( res = vp )) )
-
-
-sp = SpApiFFI( _spapi_cdef.cdef, '../build/sp.so' )
+global sp,cs
+sp = SpApiFFI( _spapi_cdef.cdef, sp_dl )
 cs = CharString( sp.ffi, sp.lib )
+enc = cs.encode
+dec = cs.decode
 
-_ = cs.encode
-__ = cs.decode
+def sp_str ( vp ):
+    #global sp
+    return sp.f.string( sp.f.cast("char *", vp) )
+
+def cs_test():
+    print "encode decode string == %s" % dec(enc( "string") )
+    
+global null
 null = sp.ffi.NULL
 
-print ""
-print "test char string codec string == %s" %__( _( "string") )
 
-def init_env_ctl_db():
-    print "init_env_ctl_db"
-    global env,ctl,db
-
+def sp_init():
     env = sp.env()
-    print env
-    print sp_type(sp,env)
-
-    ctl = sp.ctl(env = env )
-    print ctl
-    print sp_type(sp,ctl)
-
-    r = sp.set( ctl, _( SP_PATH ), _( sp_path ) )
-    print "set env path", r
-    r = sp.set( ctl, _( SP_DB ), _( db_name ) )
-    print "set db name", r
-    db = sp.get( ctl, _( db_key ) )
-
-    print db
-    print sp_type(sp,db)
-
-
-def open_env_destroy_env():
-    print "open_env_destroy_env"
-    global env
-    print "open env"
-    r = sp.open( env = env )
+    print sp_str( sp.type(env) )
+    ctl = sp.ctl( env )
+    print sp_str( sp.type(ctl) )
+    r = sp.set( ctl, enc(SP_PATH), enc(sp_path) )
+    print r 
+    r = sp.set( ctl, enc(SP_DB), enc(sp_db) )
     print r
-    print "destroy env"
-    r = sp.destroy( res = env )
-    print r
+    db = sp.get( ctl, enc(db_key) )
+    print sp_str( sp.type(db) ) , db
 
-def open_env_destroy_db_destroy_env():
-    print "open_db_destroy_db_destroy_env"
-    global env,db
-    print "open db"
-    r = sp.open(env = env )
-    print r
-    print "destroy db"
-    r = sp.destroy( res = db )
-    print  r
-    print  "destroy env"
-    r = sp.destroy( res = env )
-    print r
-
-
-# create test cases
-def open_db_destroy_db_destroy_env():
-    print "open_db_destroy_db_destroy_env"
-    print "open db" 
-    r = sp.open(db = db )
-    print r
-    print "destroy db"
-    r = sp.destroy( res = db )
-    print  r
-    print  "destroy env"
-    r = sp.destroy( res = env )
-    print r
-
-
-
-
-
-init_env_ctl_db()
-open_env_destroy_env()
-init_env_ctl_db()
-open_db_destroy_db_destroy_env()
-init_env_ctl_db()
-open_env_destroy_db_destroy_env()
+cs_test()
+sp_init()
 
