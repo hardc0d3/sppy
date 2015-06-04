3c472d8d1ed4dcfcb22e46b2d8c1410e8422de4d
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-06-04 10:44:14 +0300

    add test suite

--- sppy_unit_test/dict/single_db_u32_run.py
+++ sppy_unit_test/dict/single_db_u32_run.py
@@ -1,9 +1,22 @@
 import unittest
-import single_db_create_env
-import single_db_open_exist_env
-import single_db_create_env_load_u32
-import single_db_open_exist_env_read_u32
- 
 
-if __name__ == '__main__':
-    unittest.main()
+testmodules = [
+    'single_db_create_env'
+   ,'single_db_create_env_load_u32'
+   ,'single_db_open_exist_env'
+   ,'single_db_open_exist_env_read_u32'
+    ]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+    try:
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
+
+
