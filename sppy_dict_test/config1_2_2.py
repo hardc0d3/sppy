2a9efd97cfa761c44d1843ea9cb804a6d2217ceb
Dobri Stoilov <dobri.stoilov@ontotext.com>
2015-06-11 00:15:41 +0300

    fix old test to point submodule

--- sppy_dict_test/config1_2_2.py
+++ sppy_dict_test/config1_2_2.py
@@ -5,7 +5,7 @@ from sppy.spapi_cffi_codecs import *
 #:from sppy.spapi_cursor_dict import SophiaCursorDict
 default_sp_path = '../test_data'
 default_db_name = 'test'
-sp = SophiaApi( '../../sophia/sophia1.2.2/libsophia.so.1.2.2',sophia_api_cdefs['1.2.2'] )
+sp = SophiaApi( '../lib/sophia/libsophia.so.1.2.2',sophia_api_cdefs['1.2.2'] )
 codec_u32 = U32(sp.ffi)
 dict_db_default_config = {
 'sp.api':sp
